8a952700a6c639768333a7e910123cf15d6faa6a
Ángel Díaz <diazangel9311@gmail.com>
2021-09-20 15:31:19 -0500

    Add input field to enter new todos

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import TodosList from './TodosList';
 import Header from "./Header";
+import InputTodo from "./InputTodo";
 
 class TodoContainer extends React.Component {
   state = {
@@ -23,15 +24,46 @@ class TodoContainer extends React.Component {
     ],
   };
 
-  handleChange = (id) => {
-    console.log("clicked", id);
+  handleChange = id => {
+    this.setState(prevState => {
+      return {
+        todos: prevState.todos.map(todo => {
+          if (todo.id === id) {
+            return {
+              ...todo,
+              completed: !todo.completed,
+            }
+          }
+          return todo
+        }),
+      }
+    })
+  };
+
+  delTodo = id => {
+    this.setState({
+      todos: [
+        ...this.state.todos.filter(todo => {
+          return todo.id !== id;
+        })
+      ]
+    });
+  };
+
+  addTodoItem = title => {
+    console.log(title);
   };
 
   render() {
     return (
       <div>
         <Header />
-        <TodosList todos={this.state.todos} handleChangeProps={this.handleChange} />
+        <InputTodo />
+        <TodosList 
+          todos={this.state.todos} 
+          handleChangeProps={this.handleChange}
+          deleteTodoProps={this.delTodo}
+        />
       </div>
     );
   }
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,6 +3,9 @@ import React from "react";
 function TodoItem(props) {
   return (
     <li>
+      <button onClick={() => props.deleteTodoProps(props.todo.id)}>
+        Delete
+      </button>
       <input 
         type="checkbox" 
         checked={props.todo.completed}
